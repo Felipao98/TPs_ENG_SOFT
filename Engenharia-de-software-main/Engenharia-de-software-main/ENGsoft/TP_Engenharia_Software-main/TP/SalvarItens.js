001b792b59be332193308340cab2fa493f1ea0a0
Felipao98 <80127519+Felipao98@users.noreply.github.com>
2023-05-04 22:14:57 -0300

    Delete SalvarItens.js

--- Engenharia-de-software-main/Engenharia-de-software-main/ENGsoft/TP_Engenharia_Software-main/TP/SalvarItens.js
+++
@@ -1,9 +0,0 @@
-function salvarValor(campo) {
-    var valor = campo.value;
-    var nomeCampo = campo.getAttribute("name");
-    
-    // Salva o valor no localStorage ou sessionStorage
-    localStorage.setItem(nomeCampo, valor);
-    //sessionStorage.setItem(nomeCampo, valor);
-  }
-  
